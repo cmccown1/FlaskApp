dc3f1d4a22b996282ee4feb17cc385582967be25
Chris McCown <cmccown1@gmail.com>
2021-12-31 14:27:33 -0500

    added a page for kinzley w templating

--- FlaskApp.py
+++ FlaskApp.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('basic.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/kinzley')
+def kinzley():
+    return render_template('kinzley.html')
 
 if __name__ == '__main__':
     app.run()
